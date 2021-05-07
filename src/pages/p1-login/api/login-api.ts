7c66608abc8808fa114a47d870ca7ddcef501e6c
Natalia K <nataliaandkirik@gmail.com>
2021-05-07 17:02:09 +0300

    add login page,display email on profile page

--- src/pages/p1-login/api/login-api.ts
+++ src/pages/p1-login/api/login-api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {LoginParamsType} from "../bll/loginReducer";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0/',
 })
 
 export const authAPI = {
