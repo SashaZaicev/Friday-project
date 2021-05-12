1e5863792a9521212618b552bfd05d502845a5a8
Sasha Zaitsau <46328412+SashaZaicev@users.noreply.github.com>
2021-05-12 21:38:35 +0300

    Add pagination and range slider

--- src/pages/p8-tableFilter/bll/searchReducer.ts
+++ src/pages/p8-tableFilter/bll/searchReducer.ts
@@ -24,16 +24,29 @@ export interface ITableState {
     tableProducts: {
         items: IItemsTable[];
         settingsSearch: IItemsSettings;
+        loading: boolean
     };
 }
 
 export const InitialState: ITableState = {
     tableProducts: {
         items: [
-            {itemName: 'qwertt', price: 122, id: 'qwer112'},
-            {itemName: 'zxxxtt', price: 1332, id: 'qwer152'},
-            {itemName: 'zxxxtt', price: 1332, id: 'qwer1252'},
-            {itemName: 'dfdstt', price: 1223432, id: 'qwer122'}
+            {itemName: 'qwertt', price: 1220, id: 'qwer1121'},
+            {itemName: 'zxxxtt', price: 1332, id: 'qwer1522'},
+            {itemName: 'zxxxtt', price: 1332, id: 'qwer12523'},
+            {itemName: 'dfdstt', price: 8800, id: 'qwer1224'},
+            {itemName: 'qwertt', price: 1220, id: 'qwer1125'},
+            {itemName: 'zxxxtt', price: 1332, id: 'qwer1526'},
+            {itemName: 'zxxxtt', price: 1332, id: 'qwer12527'},
+            {itemName: 'dfdstt', price: 8800, id: 'qwer1228'},
+            {itemName: 'qwertt', price: 1220, id: 'qwer1129'},
+            {itemName: 'zxxxtt', price: 1332, id: 'qwer1520'},
+            {itemName: 'zxxxtt', price: 1332, id: 'qwer125210'},
+            {itemName: 'dfdstt', price: 8800, id: 'qwer12211'},
+            {itemName: 'qwertt', price: 1220, id: 'qwer11212'},
+            {itemName: 'zxxxtt', price: 1332, id: 'qwer15213'},
+            {itemName: 'zxxxtt', price: 1332, id: 'qwer125214'},
+            {itemName: 'dfdstt', price: 8800, id: 'qwer12215'},
         ],
         settingsSearch: {
             minPrice: 1000,
@@ -48,17 +61,19 @@ export const InitialState: ITableState = {
             productTotalCount: 7,
             page: 1,
             pageCount: 10,
-        }
+        },
+        loading: false
     },
 
 };
 const SET_SEARCH_NAME = "SET_SEARCH_NAME"
+const SET_LOADING = "SET_LOADING"
+const SET_MIN_MAX_ITEM = "SET_MIN_MAX_ITEM"
 
 
 export const searchReducer = (state = InitialState, action: any) => {
     switch (action.type) {
         case  SET_SEARCH_NAME: {
-
             return {
                 ...state,
                 tableProducts: {
@@ -69,34 +84,42 @@ export const searchReducer = (state = InitialState, action: any) => {
                 }
             }
         }
+        case SET_LOADING: {
+            return {
+                ...state,
+                tableProducts: {
+                    ...state.tableProducts,
+                    loading: action.loading
+                }
+            }
+        }
+        case SET_MIN_MAX_ITEM: {
+            return {
+                ...state,
+                tableProducts: {
+                    ...state.tableProducts,
+                    settingsSearch: {
+                        ...state.tableProducts.settingsSearch, min: action.min, max: action.max
+                    }
+                }
+            }
+        }
         default:
             return state;
     }
-};
+}
 
 export const actionsSearch = {
     setSearchName: (searchName: string) => ({type: SET_SEARCH_NAME, searchName}) as const,
+    setLoading: (loading: boolean) => ({type: SET_LOADING, loading}) as const,
+    setMinMax:(min: number, max: number) =>({type:SET_MIN_MAX_ITEM, min, max}) as const
 }
 
-export const getSearch = () => {
+export const getSearch = (newPage?: number, newPageCount?: number) => {
     return (dispatch: Dispatch) => {
-        console.log()
-        // request.getSearch(searchName).then(response => {
-        //     if (response === 0) {
-        //         dispatch(acceptFollow(userId))
-        //     }
-        //     dispatch(toggleFollowingProgress(false, userId))
+        dispatch(actionsSearch.setLoading(true))
+        // console.log('helo')
+        // tableApi.getTableItems().then(response=>{
         // })
     }
 }
-// export const follow = (userId: number) => {
-//     return (dispatch: Dispatch) => {
-//         dispatch(toggleFollowingProgress(true, userId))
-//         usersApi.follow(userId).then(response => {
-//             if (response === 0) {
-//                 dispatch(acceptFollow(userId))
-//             }
-//             dispatch(toggleFollowingProgress(false, userId))
-//         })
-//     }
-// }
--- src/pages/p8-tableFilter/ui/SearchTable.tsx
+++ src/pages/p8-tableFilter/ui/SearchTable.tsx
@@ -3,60 +3,65 @@ import s from './search.module.css'
 import {actionsSearch, getSearch} from "../bll/searchReducer";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../../app/store";
+import MultiRangeSlider from "./MultiRangeSlider/MultiRangeSlider";
+import Pagination from "./Pagination/Pagination";
 
 
 const SearchTable = () => {
     const dispatch = useDispatch()
     const {tableProducts} = useSelector((state: AppRootStateType) => state.search)
-
-    const tableSearch = (searchName:string) => {
+    const {page, pageCount, productTotalCount} = tableProducts.settingsSearch
+    const tableSearch = (searchName: string) => {
         dispatch(actionsSearch.setSearchName(searchName))
     }
     const getResult = () => {
         dispatch(getSearch())
     }
+    const getPage = (newPage: number, newPageCount: number) => {
+        dispatch(getSearch(newPage, newPageCount))
+    };
 
-    // const newItem = tableProducts.items.map(el=><div>{el.itemName}{el.price}</div>)
+    const currentItems = tableProducts.items.map(el => <tr key={el.id}>
+        <td>{el.itemName}</td>
+        <td>{el.price}</td>
+        <td></td>
+    </tr>)
     return (
         <div className={s.searchTable}>
             <div>
                 <input className={s.inputSearch}
                        value={tableProducts.settingsSearch.searchName}
-                       onChange={(e)=>tableSearch(e.currentTarget.value)} type="text"
+                       onChange={(e) => tableSearch(e.currentTarget.value)} type="text"
                        placeholder={'Введите значение для поиска'}
                 />
             </div>
-            <button onClick={getResult}>Поиск (Search)</button>
-            {tableProducts.items.filter(el=>(el.itemName===tableProducts.settingsSearch.searchName))
-                .map((item)=>(<div>{item.itemName}</div>))}
+            <div><MultiRangeSlider/></div>
+            <div>
+                <button onClick={getResult}>Поиск (Search)</button>
+            </div>
+            <div className={s.tableBlock}>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>itemName</th>
+                        <th>price</th>
+                        <th>
+                            <button>КНОПОЧКА-ПУСТЫШКА</button>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {currentItems}
+                    </tbody>
+                    <Pagination page={page} pageCount={pageCount} productTotalCount={productTotalCount}getPage={getPage}/>
+                </table>
+            </div>
+            {/*<div>{tableProducts.loading ? 'LOADING...' : 'FALSE'}*/}
+            {/*{tableProducts.items.filter(el=>(el.itemName===tableProducts.settingsSearch.searchName))*/}
+            {/*    .map((item)=>(<div>{item.itemName}</div>))}</div>*/}
         </div>
     );
 };
 
 
-export default SearchTable;
-
-
-//
-// import React, { Component } from "react";
-//
-// export default function UncontrolledFormComponent() {
-//     let inputRef = React.createRef();
-//     let name = "";
-//     const handleClick = e => {
-//         e.preventDefault();
-//         alert("Name is: " + inputRef.current.value);
-//     };
-//
-//     return (
-//         <div>
-//             <h3>Uncontrolled Form Component</h3>
-//             <form>
-//                 <input type="text" ref={inputRef} />
-//                 <button style={{ margin: "8px" }} onClick={handleClick}>
-//                     Submit
-//                 </button>
-//             </form>
-//         </div>
-//     );
-// }
+export default SearchTable
